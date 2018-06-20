75d3de4edd3a64b84697bfd5a25b4e6fc5d44bc5
Alexander Rusak <375298388602@mail.ru>
2018-06-21 00:15:56 +0300

    first step add Gulp sass and other 1.0.0.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var
- gulp  = require("gulp"),
- livereload = require("gulp-livereload");
- sass      =require("gulp-sass");
+ gulp  		   =require("gulp"),
+ livereload    =require("gulp-livereload");
+ sass      	   =require("gulp-sass");
+ autoprefixer  =require("gulp-autoprefixer");
+ cleancss	   =require("gulp-clean-css");
+ rename		   =require("gulp-rename");
  
 gulp.task("reload-css", function() {
  gulp.src('./src/*.scss')
  .pipe(sass().on('error', sass.logError))
+ .pipe(autoprefixer({
+ 	browsers:['last 3 version'],
+ 	cascade:false
+ }))
+ .pipe(gulp.dest('./src/css/'))
+ .pipe(cleancss({compatibility:'ie8'}))
+ .pipe(rename({suffix:'.min'}))
+ .pipe(livereload())
  .pipe(gulp.dest('./src/css/'))
  .pipe(livereload());
 });
