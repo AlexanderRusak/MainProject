01dbc9ca6c6fd65804dcf1315939eff30d3a49ca
Alexander Rusak <375298388602@mail.ru>
2018-07-11 15:22:47 +0300

    Made form of registration 1.0.0.2.3

--- src/JS/scripts.js
+++ src/JS/scripts.js
@@ -2,12 +2,21 @@
 
 		$(".menu a:nth-child(1)").click(function(){
 			$(".SignIn").css("display","inline-block");
-
+			$(".SignIn inputяи").focus();
 		});
 	
 		$(".menu a:nth-child(2) ").click(function(){
 			$(".SignIn").css("display","none");
 
+		});
+		$(".SignIn input:nth-child(3)").click(function(){
+				if (!$("#username").val()=="") {
+					alert("ok");
+				}
+				else{
+					alert("neok");
+				}
+				
 		});
 	});
 		
