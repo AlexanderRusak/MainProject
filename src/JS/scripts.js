386066517f6a3b5b03066bfd6b51ece80f664b16
Alexander Rusak <375298388602@mail.ru>
2018-07-12 12:18:28 +0300

    Made slider with jquerry 1.0.0.3.0

--- src/JS/scripts.js
+++ src/JS/scripts.js
@@ -1,5 +1,26 @@
 	$("document").ready(function(){
 
+		$(".next").click(function(){
+			var currentImage=$(".img.curry");
+			var currentImageIndex=$(".img.curry").index();
+			var nextImageIndex=currentImageIndex+1;
+			var nextImage=$(".img").eq(nextImageIndex);
+			currentImage.hide();
+			currentImage.removeClass("curry");
+
+			if (nextImageIndex==($(".img:last").index()+1)) {
+				$(".img").eq(0).fadeIn(1000);
+				$(".img").eq(0).addClass("curry");
+			}
+			else{
+				nextImage.fadeIn(1000);
+				nextImage.addClass("curry");
+			}
+		});		
+
+
+
+
 		$(".menu a:nth-child(1)").click(function(){
 			$(".SignIn").css("display","inline-block");
 			$(".SignIn inputяи").focus();
