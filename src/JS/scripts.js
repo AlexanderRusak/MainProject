bf4d92a015739b6166ccdd6d2f9cab78920566ff
Alexander Rusak <375298388602@mail.ru>
2018-07-13 16:08:00 +0300

    Made slider with jquerry 1.0.0.3.2

--- src/JS/scripts.js
+++ src/JS/scripts.js
@@ -1,34 +1,6 @@
 	$("document").ready(function(){
 
-		$(".next").click(function(){
-			var currentImage=$(".img.curry");
-			var currentImageIndex=$(".img.curry").index();
-			var nextImageIndex=currentImageIndex+1;
-			var nextImage=$(".img").eq(nextImageIndex);
-			currentImage.hide();
-			currentImage.removeClass("curry");
-
-			if (nextImageIndex==($(".img:last").index()+1)) {
-				$(".img").eq(0).fadeIn(1000);
-				$(".img").eq(0).addClass("curry");
-			}
-			else{
-				nextImage.fadeIn(1000);
-				nextImage.addClass("curry");
-			}
-			});	
-			$(".prev").click(function(){
-			var currentImage=$(".img.curry");
-			var currentImageIndex=$(".img.curry").index();
-			var prevImageIndex=currentImageIndex-1;
-			var prevImage=$(".img").eq(prevImageIndex);
-
-			currentImage.hide();
-			currentImage.removeClass("curry");
-			prevImage.fadeIn(1000);
-			prevImage.addClass("curry");	
-			});// code for slider
-
+	
 
 
 
